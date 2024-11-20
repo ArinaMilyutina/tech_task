c02cee5fc043f1e6c6d276d8ffa2db436183c37d
Arina <arina.milyutina.0005@gmail.com>
2024-11-21 01:42:40 +0300

    added sql code to create table book_genres

--- tech_task/src/main/resources/db/migration/V1__initial.sql
+++ tech_task/src/main/resources/db/migration/V1__initial.sql
@@ -134,3 +134,9 @@ CREATE TABLE genres
     id         SERIAL PRIMARY KEY,
     genre_name VARCHAR(100) NOT NULL UNIQUE
 );
+CREATE TABLE book_genres (
+                             book_id BIGINT NOT NULL,
+                             genre VARCHAR(255) NOT NULL,
+                             PRIMARY KEY (book_id, genre),
+                             FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
+);
