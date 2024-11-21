398738ee45785fadf0fd2097aaa7c61c76af03c8
Arina <arina.milyutina.0005@gmail.com>
2024-11-21 22:42:41 +0300

    added @RequestMapping("api/v1/members") to process http request and added memberService

--- tech_task/src/main/java/com/ifortex/bookservice/controller/MemberController.java
+++ tech_task/src/main/java/com/ifortex/bookservice/controller/MemberController.java
@@ -1,26 +1,29 @@
 package com.ifortex.bookservice.controller;
 
 import com.ifortex.bookservice.model.Member;
-import com.ifortex.bookservice.service.MemberService;
+import com.ifortex.bookservice.service.impl.MyESMemberServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("api/v1/members")
+@RestController
+@RequestMapping("api/v1/members")
 @AllArgsConstructor
 public class MemberController {
 
-  private final MemberService memberService;
+    private final MyESMemberServiceImpl memberService;
 
-  @GetMapping("amateur")
-  public Member findMember() {
-    return memberService.findMember();
-  }
 
-  @GetMapping
-  public List<Member> findMembers() {
-    return memberService.findMembers();
-  }
+    @GetMapping("amateur")
+    public Member findMember() {
+        return memberService.findMember();
+    }
+
+    @GetMapping
+    public List<Member> findMembers() {
+        return memberService.findMembers();
+    }
 }
